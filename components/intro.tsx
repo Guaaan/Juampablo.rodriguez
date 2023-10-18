5741bea8888cf491813064d90c05151c4c622719
Juan Rodriguez <juan.rodriguez@redfarma.cl>
2023-10-18 19:56:44 -0300

    Juampablog

--- components/intro.tsx
+++ components/intro.tsx
@@ -5,7 +5,7 @@ const Intro = () => {
   return (
     <section className="flex-col md:flex-row flex items-center md:justify-between mt-16 mb-16 md:mb-12">
       <h1 className="text-5xl md:text-8xl font-bold tracking-tighter leading-tight md:pr-8">
-        ZamuroAtomico
+        Juampablog
       </h1>
       <div className="text-center md:text-left text-lg mt-5 md:pl-8">
         repositorio de creación digital.
@@ -20,7 +20,7 @@ const Intro = () => {
             .callFunction(() => {
               console.log('All strings were deleted');
             })
-            .start();
+           .start();
         }}
       /> */}
       </div>
